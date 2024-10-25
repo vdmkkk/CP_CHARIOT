355f68b3d093eb496e6de08076e41f2bfdf88cee
vdmkkk <soloviov.vad@gmail.com>
2024-10-26 01:05:23 +0300

    init :)

---
+++ examples/src/App.vue
@@ -0,0 +1,9 @@
+<template class="no-scroll overflow-hidden">
+  <router-view class="no-scroll overflow-hidden" />
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'App',
+});
+</script>
---
+++ examples/src/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page ref="pageRef" style="width: 100%" class="full-height no-scroll">
+    <iframe
+      class="fullscreen-frame no-scroll"
+      src="http://localhost:9000"
+      :style="{ height: pageHeight }"
+      frameborder="0"
+    ></iframe>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, useTemplateRef } from 'vue';
+const pageContainerRef = useTemplateRef('pageRef');
+const pageHeight = ref(0);
+
+onMounted(() => {
+  pageHeight.value = pageContainerRef.value?.$el.parentElement.style.minHeight;
+
+  const target = document.querySelector('body')?.style;
+  target.overflowY = 'hidden';
+});
+</script>
+
+<style lang="scss" scoped>
+.fullscreen-frame {
+  width: 100%;
+  height: 819px;
+}
+</style>
